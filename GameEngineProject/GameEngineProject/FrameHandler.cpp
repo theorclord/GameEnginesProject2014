552a146a28ddb11be3aa9517b967f463cafa519a
Tornady <clbw@itu.dk>
2014-11-06 13:52:25 +0100

    added my FrameHandler stuff back in

--- GameEngineProject/GameEngineProject/FrameHandler.cpp
+++ GameEngineProject/GameEngineProject/FrameHandler.cpp
@@ -16,25 +16,36 @@ void FrameHandler::AddSprite(Sprite sprite)
 	FrameHandler::spriteList.assign(1,sprite);
 }
 
-void FrameHandler::CreateFrame(int width,int height,string title){
-	sf::RenderWindow window(sf::VideoMode(width, height), title);
+void FrameHandler::CreateFrame(int width, int height, string title){
+	window.create(sf::VideoMode(width, height), title, sf::Style::Close);
+	Update();
+}
 
-		
+void FrameHandler::Resize(int width, int height) {
+	window.setSize(sf::Vector2u(width, height));
+}
+
+void FrameHandler::Update(void)
+{
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+	shape.setPosition(50, 50);
 	while (window.isOpen())
 	{
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
+			// Added resize testing events..
 			if (event.type == sf::Event::Closed)
 				window.close();
+			if (event.type == sf::Event::MouseButtonPressed)
+				Resize(500, 500);
+			if (event.type == sf::Event::KeyPressed)
+				Resize(200, 200);
 		}
-		
+
 		window.clear();
 		window.draw(shape);
 		window.display();
 	}
-		
-		
 }
