5d26de25268981f3925e5c9f1884b36e29e184cb
theorclord <mikkeltheorclord@hotmail.com>
2014-12-02 19:23:23 +0100

    added data to circle collider

--- GameEngineProject/GameEngineProject/CircleCollider.h
+++ GameEngineProject/GameEngineProject/CircleCollider.h
@@ -1,16 +1,20 @@
 #pragma once
 #include "stdafx.h"
 #include <tuple>
+//#include "Transform.h"
 
 class CircleCollider
 {
 	private:
-		//tuple<float,float> center;
+		std::tuple<float,float> center;
 		float radius;
 		bool stay;
 	public:
 		bool DetectCollision(CircleCollider coll, float dist);
 		float getRadius();
 		void setRadius(float rad);
+		std::tuple<float, float> getCenter();
+		void setCenter(std::tuple<float,float> center);
 		virtual void OnEnter();
+		
 };
